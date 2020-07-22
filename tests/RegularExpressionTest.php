99be9a86e921ce5040fd04c42e9f9227012a98c5
Nathanael <nathanael@shermett.me>
2020-07-22 14:53:20 -0400

    Initial commit.

---
+++ tests/RegularExpressionTest.php
@@ -0,0 +1,58 @@
+<?php
+
+use Theorem\Accidental;
+use Theorem\RegularExpression;
+use PHPUnit\Framework\TestCase;
+
+class RegularExpressionTest extends TestCase
+{
+	public function testParseScientificNoteNotation()
+	{
+		$regex = RegularExpression::ParseScientificNoteNotation('A4', $output);
+		$this->assertEquals(TRUE, $regex);
+		$this->assertEquals('A', $output['letter']);
+		$this->assertInstanceOf(Accidental\Natural::class, $output['accidental']);
+		$this->assertEquals(4, $output['octave']);
+		unset($output);
+
+		$regex = RegularExpression::ParseScientificNoteNotation('Ab4', $output);
+		$this->assertEquals(TRUE, $regex);
+		$this->assertEquals('A', $output['letter']);
+		$this->assertInstanceOf(Accidental\Flat::class, $output['accidental']);
+		$this->assertEquals(4, $output['octave']);
+		unset($output);
+
+		$regex = RegularExpression::ParseScientificNoteNotation('Abb4', $output);
+		$this->assertEquals(TRUE, $regex);
+		$this->assertEquals('A', $output['letter']);
+		$this->assertInstanceOf(Accidental\DoubleFlat::class, $output['accidental']);
+		$this->assertEquals(4, $output['octave']);
+		unset($output);
+
+		$regex = RegularExpression::ParseScientificNoteNotation('A-4', $output);
+		$this->assertEquals(TRUE, $regex);
+		$this->assertEquals('A', $output['letter']);
+		$this->assertInstanceOf(Accidental\Natural::class, $output['accidental']);
+		$this->assertEquals(-4, $output['octave']);
+		unset($output);
+
+		$regex = RegularExpression::ParseScientificNoteNotation('C4', $output);
+		$this->assertEquals(TRUE, $regex);
+		$this->assertEquals('C', $output['letter']);
+		$this->assertInstanceOf(Accidental\Natural::class, $output['accidental']);
+		$this->assertEquals(4, $output['octave']);
+		unset($output);
+
+		$regex = RegularExpression::ParseScientificNoteNotation('A0', $output);
+		$this->assertEquals(TRUE, $regex);
+		$this->assertEquals('A', $output['letter']);
+		$this->assertInstanceOf(Accidental\Natural::class, $output['accidental']);
+		$this->assertEquals(0, $output['octave']);
+		unset($output);
+
+		$regex = RegularExpression::ParseScientificNoteNotation('A', $output);
+		$this->assertEquals(FALSE, $regex);
+		$this->assertEquals(FALSE, isset($output));
+		unset($output);
+	}
+}
