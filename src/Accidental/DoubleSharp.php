269ff56c285657d98e598953eb4e50fe8abb30d9
Nathanael <nathanael@shermett.me>
2020-07-28 17:44:57 -0400

    Fix misnamed class.

--- src/Accidental/DoubleSharp.php
+++ src/Accidental/DoubleSharp.php
@@ -6,7 +6,7 @@ class DoubleSharp extends AbstractAccidental
 {
 	public function __construct()
 	{
-		$this->setOffset(AccidentalEnum::DOUBLE_SHARP);
+		$this->setOffset(AbstractAccidental::DOUBLE_SHARP);
 	}
 
 	public function toString($outputMode = NULL, $renderMode = NULL): string
