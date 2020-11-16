22924f57f086a6753ea8748eb6d4ac592d6d6e41
Nathanael <nathanael@shermett.me>
2020-11-16 16:53:44 -0500

    Add parseAccidental(). Utilize AccidentalFactory. Add quarter tone subpattern matching.

--- src/RegularExpression.php
+++ src/RegularExpression.php
@@ -2,8 +2,29 @@
 
 namespace Theorem;
 
+use Theorem\Accidental\AccidentalFactory;
+
 abstract class RegularExpression
 {
+	private static string $letterPattern     = '(?<letter>[ABCDEFG]){1}';
+	private static string $accidentalPattern = '
+		(?<accidental>
+			(?<quarterTone>[d+]{0,1})
+			(?:
+				(?:
+					(?:
+						b*
+					)
+				)|
+				(?:
+					\#{0,1}
+					x*
+				)
+			)
+		)
+	';
+	private static string $octavePattern     = '(?<octave>-*[1-9]+[0-9]*|0)';
+
 	/**
 	 * Parses the string representation of a note (in scientific pitch notation) into an array consisting of a letter,
 	 * an accidental, and an octave.
@@ -13,38 +34,42 @@ abstract class RegularExpression
 	 * @return bool|array Returns an associative array containing a letter (string), an accidental
 	 *                      ({@see AbstractAccidental} sub-type), and an octave (int).
 	 */
-	public static function ParseScientificNoteNotation(string $input, &$output)
+	public static function parseScientificNoteNotation(string $input, &$output)
 	{
 		// Match a note expressed in scientific pitch notation.
-		$regex = '/^
-			(?<letter>[ABCDEFG]){1}
-			(?<accidental>bb|[b#x]){0,1}
-			(?<octave>-*[1-9]+[0-9]*|0)
-		$/x';
+		$regex = '/
+			^
+			' . self::$letterPattern . '
+			' . self::$accidentalPattern . '
+			' . self::$octavePattern . '
+			$/x';
 
 		// If there was a match.
 		if (preg_match($regex, $input, $result) === 1)
 		{
 			// Convert the matched accidental to an accidental object.
-			switch ($result['accidental'])
-			{
-				case 'bb':
-					$result['accidental'] = new Accidental\DoubleFlat();
-					break;
-				case 'b':
-					$result['accidental'] = new Accidental\Flat();
-					break;
-				case '#':
-					$result['accidental'] = new Accidental\Sharp();
-					break;
-				case 'x':
-					$result['accidental'] = new Accidental\DoubleSharp();
-					break;
-				default:
-					$result['accidental'] = new Accidental\Natural();
-					break;
-			}
+			$accidentalFactory = new AccidentalFactory();
+			$accidentalFactory->createFromString($result['accidental']);
 
+			$result['accidental'] = $accidentalFactory->build();
+			$output = $result;
+
+			return TRUE;
+		}
+		else
+		{
+			return FALSE;
+		}
+	}
+
+	public static function parseAccidental(string $input, &$output)
+	{
+		// Match a note expressed in scientific pitch notation.
+		$regex = '/^' . self::$accidentalPattern . '$/x';
+
+		// If there was a match.
+		if (preg_match($regex, $input, $result) === 1)
+		{
 			$output = $result;
 
 			return TRUE;
