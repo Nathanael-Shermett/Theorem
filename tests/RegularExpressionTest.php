1c14af11b9b9e57318b7d24b67dae165931e832c
Nathanael <nathanael@shermett.me>
2020-11-16 17:10:16 -0500

    Update tests to use AccidentalFactory.

--- tests/RegularExpressionTest.php
+++ tests/RegularExpressionTest.php
@@ -2,72 +2,72 @@
 
 use PHPUnit\Framework\TestCase;
 use Theorem\Accidental;
+use Theorem\Accidental\AccidentalFactory;
 use Theorem\RegularExpression;
 
 class RegularExpressionTest extends TestCase
 {
 	public function testParseScientificNoteNotation()
 	{
-		$regex = RegularExpression::ParseScientificNoteNotation('A4', $output);
+		$accidentalFactory = new AccidentalFactory();
+
+		$regex = RegularExpression::parseScientificNoteNotation('A4', $output);
 		$this->assertTrue($regex);
 		$this->assertEquals('A', $output['letter']);
-		$this->assertInstanceOf(Accidental\Natural::class, $output['accidental']);
+		$this->assertInstanceOf(Accidental\Natural::class, $accidentalFactory->createFromString($output['accidental'])
+																			 ->build());
 		$this->assertEquals(4, $output['octave']);
 		unset($output);
 
-		$regex = RegularExpression::ParseScientificNoteNotation('Ab4', $output);
+		$regex = RegularExpression::parseScientificNoteNotation('Cdb-4', $output);
 		$this->assertTrue($regex);
-		$this->assertEquals('A', $output['letter']);
-		$this->assertInstanceOf(Accidental\Flat::class, $output['accidental']);
-		$this->assertEquals(4, $output['octave']);
-		unset($output);
-
-		$regex = RegularExpression::ParseScientificNoteNotation('Abb4', $output);
-		$this->assertTrue($regex);
-		$this->assertEquals('A', $output['letter']);
-		$this->assertInstanceOf(Accidental\DoubleFlat::class, $output['accidental']);
-		$this->assertEquals(4, $output['octave']);
-		unset($output);
-
-		$regex = RegularExpression::ParseScientificNoteNotation('A-4', $output);
-		$this->assertTrue($regex);
-		$this->assertEquals('A', $output['letter']);
-		$this->assertInstanceOf(Accidental\Natural::class, $output['accidental']);
+		$this->assertEquals('C', $output['letter']);
+		$this->assertInstanceOf(Accidental\ThreeQuarterFlat::class, $accidentalFactory->createFromString($output['accidental'])
+																					  ->build());
 		$this->assertEquals(-4, $output['octave']);
 		unset($output);
 
-		$regex = RegularExpression::ParseScientificNoteNotation('C4', $output);
+		$regex = RegularExpression::parseScientificNoteNotation('Gbbb4', $output);
 		$this->assertTrue($regex);
-		$this->assertEquals('C', $output['letter']);
-		$this->assertInstanceOf(Accidental\Natural::class, $output['accidental']);
+		$this->assertEquals('G', $output['letter']);
+		$this->assertInstanceOf(Accidental\TripleFlat::class, $accidentalFactory->createFromString($output['accidental'])
+																				->build());
+		$this->assertEquals(4, $output['octave']);
+		unset($output);
+		$regex = RegularExpression::parseScientificNoteNotation('Gbbbb4', $output);
+		$this->assertTrue($regex);
+		$this->assertEquals('G', $output['letter']);
+		$this->assertInstanceOf(Accidental\Special::class, $accidentalFactory->createFromString($output['accidental'])
+																			 ->build());
 		$this->assertEquals(4, $output['octave']);
 		unset($output);
 
-		$regex = RegularExpression::ParseScientificNoteNotation('A0', $output);
+		$regex = RegularExpression::parseScientificNoteNotation('F+x-8', $output);
 		$this->assertTrue($regex);
-		$this->assertEquals('A', $output['letter']);
-		$this->assertInstanceOf(Accidental\Natural::class, $output['accidental']);
-		$this->assertEquals(0, $output['octave']);
+		$this->assertEquals('F', $output['letter']);
+		$this->assertInstanceOf(Accidental\FiveQuarterSharp::class, $accidentalFactory->createFromString($output['accidental'])
+																					  ->build());
+		$this->assertEquals(-8, $output['octave']);
 		unset($output);
 
-		$regex = RegularExpression::ParseScientificNoteNotation('A', $output);
+		$regex = RegularExpression::parseScientificNoteNotation('A', $output);
 		$this->assertFalse($regex);
-		$this->assertFalse(isset($output));
 		unset($output);
 
-		$regex = RegularExpression::ParseScientificNoteNotation('4', $output);
+		$regex = RegularExpression::parseScientificNoteNotation('b', $output);
 		$this->assertFalse($regex);
-		$this->assertFalse(isset($output));
 		unset($output);
 
-		$regex = RegularExpression::ParseScientificNoteNotation('Abbb4', $output);
+		$regex = RegularExpression::parseScientificNoteNotation('4', $output);
 		$this->assertFalse($regex);
-		$this->assertFalse(isset($output));
 		unset($output);
 
-		$regex = RegularExpression::ParseScientificNoteNotation('A##4', $output);
+		$regex = RegularExpression::parseScientificNoteNotation('A##4', $output);
 		$this->assertFalse($regex);
-		$this->assertFalse(isset($output));
+		unset($output);
+
+		$regex = RegularExpression::parseScientificNoteNotation('Abb4', $output);
+		$this->assertTrue($regex);
 		unset($output);
 	}
 }
