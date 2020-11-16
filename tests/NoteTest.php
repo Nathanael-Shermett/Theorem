33e4dc4551791c8c057a13918580e9155124e4e9
Nathanael <nathanael@shermett.me>
2020-11-16 17:13:46 -0500

    Update tests to use quarter tones.

--- tests/NoteTest.php
+++ tests/NoteTest.php
@@ -10,19 +10,23 @@ class NoteTest extends TestCase
 	public function testDistanceTo()
 	{
 		Setting::setStep(SETTING::QUARTER_TONE);
-		$this->assertEquals(2, (new Note('A4'))->distanceTo(new Note('Bb4')));
-		$this->assertEquals(4, (new Note('A4'))->distanceTo(new Note('B4')));
-		$this->assertEquals(-18, (new Note('A4'))->distanceTo(new Note('C4')));
+		$this->assertEquals(1.0, (new Note('Ad4'))->distanceTo(new Note('A4')));
+		$this->assertEquals(-2.0, (new Note('A+4'))->distanceTo(new Note('Ad4')));
+		$this->assertEquals(2.0, (new Note('A4'))->distanceTo(new Note('Bb4')));
+		$this->assertEquals(4.0, (new Note('A4'))->distanceTo(new Note('B4')));
+		$this->assertEquals(-18.0, (new Note('A4'))->distanceTo(new Note('C4')));
 
 		Setting::setStep(SETTING::SEMITONE);
-		$this->assertEquals(0, (new Note('B4'))->distanceTo(new Note('Cb5')));
-		$this->assertEquals(0, (new Note('B#4'))->distanceTo(new Note('C5')));
-		$this->assertEquals(1, (new Note('B4'))->distanceTo(new Note('C5')));
+		$this->assertEquals(1.5, (new Note('Bdb4'))->distanceTo(new Note('Cb5')));
+		$this->assertEquals(0.0, (new Note('B4'))->distanceTo(new Note('Cb5')));
+		$this->assertEquals(0.0, (new Note('B#4'))->distanceTo(new Note('C5')));
+		$this->assertEquals(1.0, (new Note('B4'))->distanceTo(new Note('C5')));
 
 		Setting::setStep(SETTING::WHOLE_TONE);
-		$this->assertEquals(0, (new Note('C4'))->distanceTo(new Note('C4')));
+		$this->assertEquals(0.25, (new Note('Cd4'))->distanceTo(new Note('C4')));
+		$this->assertEquals(0.0, (new Note('C4'))->distanceTo(new Note('C4')));
 		$this->assertEquals(1.5, (new Note('C4'))->distanceTo(new Note('D#4')));
-		$this->assertEquals(1, (new Note('D4'))->distanceTo(new Note('E4')));
+		$this->assertEquals(1.0, (new Note('D4'))->distanceTo(new Note('E4')));
 		$this->assertEquals(1.5, (new Note('D4'))->distanceTo(new Note('E#4')));
 		$this->assertEquals(1.5, (new Note('D4'))->distanceTo(new Note('F4')));
 		$this->assertEquals(2.5, (new Note('D4'))->distanceTo(new Note('Fx4')));
